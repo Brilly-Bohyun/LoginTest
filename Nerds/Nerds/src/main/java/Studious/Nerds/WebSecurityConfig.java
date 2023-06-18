1bf375efcf139b968b2db68f656ce576c7858485
Brilly-Bohyun <bohyun564@swu.ac.kr>
2023-06-18 21:41:55 +0900

    feat: config 사항 정의
    websecurity와 jwt를 하기 위해 설정해야할 사항들을 정의함

---
+++ Nerds/Nerds/src/main/java/Studious/Nerds/WebSecurityConfig.java
@@ -0,0 +1,58 @@
+package Studious.Nerds;
+
+import Studious.Nerds.user.handler.JwtAccessDeniedHandler;
+import Studious.Nerds.user.handler.JwtAuthenticationEntryPoint;
+import Studious.Nerds.user.token.TokenProvider;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+/**
+ * Spring Security 환경 설정을 구성하기 위한 클래스입니다.
+ * 웹 서비스가 로드 될때 Spring Container 의해 관리가 되는 클래스이며 사용자에 대한 ‘인증’과 ‘인가’에 대한 구성을 Bean 메서드로 주입을 한다.
+ */
+@Slf4j
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig {
+
+    private TokenProvider tokenProvider;
+    private JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+    private JwtAccessDeniedHandler jwtAccessDeniedHandler;
+
+    // PasswordEncoder는 BCryptPasswordEncoder 사용
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        log.debug("[+] WebSecurityConfig Start !!!");
+        http
+                .csrf().disable() // 서버에 인증정보를 저장하지 않기에 csrf를 사용하지 않음
+                .exceptionHandling()
+                .authenticationEntryPoint(jwtAuthenticationEntryPoint)
+                .accessDeniedHandler(jwtAccessDeniedHandler)
+
+                // 세션을 사용하지 않기 때문에 STATELESS 로 설정
+                .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+
+                .and()
+                .authorizeRequests()
+                .anyRequest().permitAll()
+
+                .and()
+                .apply(new JwtSecurityConfig(tokenProvider)); // JwtFilter를 addFilterBefore로 등록했던 JwtSecurityConfig class 적용
+
+        return http.build();
+    }
+}
